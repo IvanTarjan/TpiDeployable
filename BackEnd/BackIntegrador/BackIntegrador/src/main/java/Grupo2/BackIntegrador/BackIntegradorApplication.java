d1e4bc2cd7de49ace5a767f10f8fb9f039a4bd9a
IvanTarjan <ivantarjan2003@gmail.com>
2023-02-14 18:22:50 -0300

    Iniciando el proyecto de spring

---
+++ BackEnd/BackIntegrador/BackIntegrador/src/main/java/Grupo2/BackIntegrador/BackIntegradorApplication.java
@@ -0,0 +1,13 @@
+package Grupo2.BackIntegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackIntegradorApplication.class, args);
+	}
+
+}
---
+++ BackEnd/BackIntegrador/BackIntegrador/src/test/java/Grupo2/BackIntegrador/BackIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package Grupo2.BackIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
