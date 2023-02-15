cbe73f085213208f1eed7cdafc40dcc29affbdbe
Emilio Pino <pinoen@yahoo.com.ar>
2023-02-15 17:26:59 -0300

    se completa pagina de registro de nuevo usuario

---
+++ FrontEnd/FrontIntegrador/src/components/pages/CreateAccount.jsx
@@ -0,0 +1,146 @@
+import React, { useContext, useState } from "react";
+import { Button, Grid, IconButton, InputAdornment, TextField, Typography } from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
+import { useFormik } from "formik";
+import * as Yup from 'yup';
+import Header from "./Header";
+import styles from '../styles/CreateAccount.module.css';
+import { HeaderContext } from "../contexts/HeaderContext";
+import { Link } from "react-router-dom";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+const CreateAccount = () => {
+
+  const { user, setUser } = useContext(HeaderContext)
+  const [isSubmit, setIsSubmit] = useState(false)
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const { handleSubmit, handleChange, values, errors } = useFormik({
+    initialValues: {
+      name: '',
+      surname: '',
+      email: '',
+      password: '',
+      confirm: '',
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required('Campo obligatorio'),
+      surname: Yup.string().required('Campo obligatorio'),
+      email: Yup.string().email("Ingresar una direccion de email valida").required('Campo obligatorio'),
+      password: Yup.string().min(7, 'La contrasena debe tener mas de 6 caracteres').required('Campo obligatorio'),
+      confirm: Yup.string().label('Confirmar el password ingresado').required('Campo obligatorio').oneOf([Yup.ref('password'), null], 'Las contrasenas deben ser iguales'),
+
+    }),
+    onSubmit: (data) => {
+      setIsSubmit(prev => !prev)
+      setUser(data)
+      console.log(user)
+    }
+  })
+
+  return (
+    <div>
+      <Header />
+
+      <form className={styles.formContainer} onSubmit={handleSubmit}>
+        <p className={styles.formTitle}>Crear cuenta</p>
+        <Grid
+          container
+          alignItems={"center"}
+          justifyContent={"space-evenly"}
+          spacing={2}
+          sx={{ width: "100%" }} >
+
+          <Grid item xs={12} md={8}>
+            <TextField
+              value={values.name}
+              onChange={handleChange}
+              name='name'
+              type={"text"}
+              label="Nombre"
+              variant="outlined"
+              error={errors.name}
+              helperText={errors.name}
+              fullWidth
+            />
+
+          </Grid>
+
+          <Grid item xs={12} md={8}>
+            <TextField
+              value={values.surname}
+              onChange={handleChange}
+              name='surname'
+              type={"text"}
+              label="Apellido"
+              variant="outlined"
+              error={errors.surname}
+              helperText={errors.surname}
+              fullWidth
+            />
+
+          </Grid>
+
+          <Grid item xs={12} md={8}>
+            <TextField
+              value={values.email}
+              onChange={handleChange}
+              name='email'
+              type={"email"}
+              label="Correo electronico"
+              variant="outlined"
+              error={errors.email}
+              helperText={errors.email}
+              fullWidth
+            />
+
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <TextField
+              InputProps={{
+                endAdornment: (
+                  <IconButton onClick={handleClickShowPassword}>
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                ),
+              }}
+              value={values.password}
+              onChange={handleChange}
+              name='password'
+              type={showPassword ? "text" : "password"}
+              label="Contrasena"
+              variant="outlined"
+              error={errors.password}
+              helperText={errors.password}
+              fullWidth
+            />
+
+          </Grid>
+
+          <Grid item xs={12} md={8}>
+            <TextField
+              value={values.confirm}
+              onChange={handleChange}
+              name='confirm'
+              type={"password"}
+              label="Confirmar contrasena"
+              variant="outlined"
+              error={errors.confirm}
+              helperText={errors.confirm}
+              fullWidth
+            />
+
+          </Grid>
+
+        </Grid>
+        <Button size='large' color="info" type='submit' variant='contained' endIcon={<SendIcon />}>Crear Cuenta</Button>
+        <p className={styles.paragraph}>Ya tienes una cuenta? <Link to={'/'}>Iniciar sesion</Link></p>
+      </form>
+    </div>
+  )
+}
+
+export default CreateAccount
