147e820cc550f21b2d75e29090d4b6f480132b9a
Emilio Pino <pinoen@yahoo.com.ar>
2023-02-15 17:31:56 -0300

    se completa componente header

--- FrontEnd/FrontIntegrador/src/components/pages/Header.jsx
+++ FrontEnd/FrontIntegrador/src/components/pages/Header.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styles from '../styles/Header.module.css'
 import logo from '../../assets/logo.svg'
+import { HeaderContext } from '../contexts/HeaderContext'
+import { useNavigate } from 'react-router-dom'
 
 
 const Header = () => {
+
+  const { headerType, setHeaderType } = useContext(HeaderContext);
+  const nagivate = useNavigate()
+  const handleCreateAccount = () => {
+    setHeaderType('createAccount')
+    nagivate('/register')
+  }
   return (
     <div className={styles.headerContainer}>
       <div className={styles.logoContainer}>
         <img src={logo} />
         <p>Viaja como quieras</p>
-
       </div>
 
-      <div className={styles.btnContainer}>
-        <button>Crear cuenta</button>
-        <button className={styles.btnFilter}>Iniciar sesion</button>
-      </div>
+      {
+        headerType === 'initial' ? <div className={styles.btnContainer}>
+          <button onClick={handleCreateAccount}>Crear cuenta</button>
+          <button className={styles.btnFilter}>Iniciar sesion</button>
+        </div> :
+          <div className={styles.btnContainer}>
+            <button className={styles.btnFilter}>Iniciar sesion</button>
+          </div>
+      }
     </div>
   )
 }
