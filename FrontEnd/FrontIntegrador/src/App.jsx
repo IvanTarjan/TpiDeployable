148adedc969b46c286247d8c3c0807dbdbd32d66
Emilio Pino <pinoen@yahoo.com.ar>
2023-02-15 17:33:12 -0300

    se agregan router a app.js

--- FrontEnd/FrontIntegrador/src/App.jsx
+++ FrontEnd/FrontIntegrador/src/App.jsx
@@ -2,12 +2,23 @@ import './App.css'
 import Header from './components/pages/Header'
 import Body from './components/pages/Body'
 import Footer from './components/pages/Footer'
+import HeaderContextProvider from './components/contexts/HeaderContext'
+import CreateAccount from './components/pages/CreateAccount'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Header />
+      <HeaderContextProvider>
+        <BrowserRouter>
+          <Header />
+          <Footer />
+          <Routes>
+            <Route path='/register' element={<CreateAccount />} ></Route>
+          </Routes>
 
+        </BrowserRouter>
+      </HeaderContextProvider>
 
     </div>
   )
