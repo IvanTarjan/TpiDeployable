4406771fb3d90839242c220aa536ce254b858e3a
Pablo Velez <pablovelezlopera3@gmail.com>
2023-02-15 19:32:34 -0500

    Backend: controller, model, repository y service avanzados

---
+++ BackEnd/BackIntegrador/BackIntegrador/src/main/java/Grupo2/BackIntegrador/model/Categoria.java
@@ -0,0 +1,60 @@
+package Grupo2.BackIntegrador.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="CATEGORIA")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String titulo;
+    @Column
+    private String descripcion;
+    @Column
+    private String url_image;
+
+    public Categoria(Long id, String titulo, String descripcion, String url_image) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.url_image = url_image;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUrl_image() {
+        return url_image;
+    }
+
+    public void setUrl_image(String url_image) {
+        this.url_image = url_image;
+    }
+}
