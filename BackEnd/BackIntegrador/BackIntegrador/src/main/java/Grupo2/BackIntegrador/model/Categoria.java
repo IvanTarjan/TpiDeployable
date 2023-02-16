1f64cd009df13a6d823dd8cbd775e64cb7f13926
miguelzapa <miguel.zapasu@gmail.com>
2023-02-16 17:49:16 -0500

    Agrego los nuevos atributos

--- BackEnd/BackIntegrador/BackIntegrador/src/main/java/Grupo2/BackIntegrador/model/Categoria.java
+++ BackEnd/BackIntegrador/BackIntegrador/src/main/java/Grupo2/BackIntegrador/model/Categoria.java
@@ -13,17 +13,48 @@ public class Categoria {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
-    private String titulo;
+    private String category;
     @Column
-    private String descripcion;
+    private String name;
     @Column
-    private String url_image;
+    private String image;
+    @Column
+    private int price;
+    @Column
+    private String model;
+    @Column
+    private String messagge;
+    @Column
+    private String gearbox;
+    @Column
+    private String trunk;
+    @Column
+    private int doors;
+    @Column
+    private boolean airConditioning;
+    @Column
+    private boolean ABSBreak;
+    @Column
+    private String fuel;
 
-    public Categoria(Long id, String titulo, String descripcion, String url_image) {
+
+    public Categoria() {
+    }
+
+    public Categoria(Long id, String category, String name, String image, int price, String model, String messagge, String gearbox, String trunk, int doors, boolean airConditioning, boolean ABSBreak, String fuel) {
         this.id = id;
-        this.titulo = titulo;
-        this.descripcion = descripcion;
-        this.url_image = url_image;
+        this.category = category;
+        this.name = name;
+        this.image = image;
+        this.price = price;
+        this.model = model;
+        this.messagge = messagge;
+        this.gearbox = gearbox;
+        this.trunk = trunk;
+        this.doors = doors;
+        this.airConditioning = airConditioning;
+        this.ABSBreak = ABSBreak;
+        this.fuel = fuel;
     }
 
     public Long getId() {
@@ -34,27 +65,99 @@ public class Categoria {
         this.id = id;
     }
 
-    public String getTitulo() {
-        return titulo;
+    public String getCategory() {
+        return category;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public void setCategory(String category) {
+        this.category = category;
     }
 
-    public String getDescripcion() {
-        return descripcion;
+    public String getName() {
+        return name;
     }
 
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getUrl_image() {
-        return url_image;
+    public String getImage() {
+        return image;
     }
 
-    public void setUrl_image(String url_image) {
-        this.url_image = url_image;
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMessagge() {
+        return messagge;
+    }
+
+    public void setMessagge(String messagge) {
+        this.messagge = messagge;
+    }
+
+    public String getGearbox() {
+        return gearbox;
+    }
+
+    public void setGearbox(String gearbox) {
+        this.gearbox = gearbox;
+    }
+
+    public String getTrunk() {
+        return trunk;
+    }
+
+    public void setTrunk(String trunk) {
+        this.trunk = trunk;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public boolean isAirConditioning() {
+        return airConditioning;
+    }
+
+    public void setAirConditioning(boolean airConditioning) {
+        this.airConditioning = airConditioning;
+    }
+
+    public boolean isABSBreak() {
+        return ABSBreak;
+    }
+
+    public void setABSBreak(boolean ABSBreak) {
+        this.ABSBreak = ABSBreak;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
     }
 }
