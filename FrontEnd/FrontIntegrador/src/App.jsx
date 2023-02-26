900478bdfaba8b311fabcb067b78d10b17aac1ae
IvanTarjan <ivantarjan2003@gmail.com>
2023-02-26 18:20:30 -0300

    Empezando a hacer la pagina de detalles del producto, ya termine el enrutado y avance con el header, la descripcion y estoy empezando con las caracteristicas

--- FrontEnd/FrontIntegrador/src/App.jsx
+++ FrontEnd/FrontIntegrador/src/App.jsx
@@ -7,6 +7,7 @@ import CreateAccount from './components/pages/CreateAccount'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './components/pages/Login'
 import Palette from './components/contexts/ThemeContext'
+import DetallesProducto from './components/pages/DetallesProducto'
 
 function App() {
   return (
@@ -16,8 +17,9 @@ function App() {
           <BrowserRouter>
             <Header />
             <Routes>
-              <Route path='/register' element={<CreateAccount />} ></Route>
+              <Route path='/register' element={<CreateAccount />}/>
               <Route path='/login' element={<Login />} />
+              <Route path='/producto/:id' element={<DetallesProducto />} />
               <Route path='/' element={<Body />} />
             </Routes>
             <Footer />
