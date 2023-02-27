a479c8dd55a728dfe01fe0ca104748824bb58399
Emilio Pino <pinoen@yahoo.com.ar>
2023-02-27 12:41:49 -0300

    se crea context para data a renderizar en body components

---
+++ FrontEnd/FrontIntegrador/src/components/contexts/BodyContext.jsx
@@ -0,0 +1,26 @@
+import React, { createContext, useEffect, useState } from 'react'
+import axios from 'axios'
+
+export const BodyContext = createContext()
+
+const BodyContextProvider = ({ children }) => {
+  const [cars, setCars] = useState([])
+  useEffect(() => {
+    axios.get("http://localhost:5000/cars")
+      .then(res => setCars(res.data))
+      .catch(err => console.log(err))
+  }, [])
+
+  const data = {
+    cars,
+    setCars
+  }
+
+  return (
+    <BodyContext.Provider value={data}>
+      {children}
+    </BodyContext.Provider>
+  )
+}
+
+export default BodyContextProvider
