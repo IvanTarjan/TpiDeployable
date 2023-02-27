b922f1778f73fb845a6a377d8743c78bacd17e7a
Emilio Pino <pinoen@yahoo.com.ar>
2023-02-27 20:14:56 -0300

    se agrega al body context state para localizaciones

--- FrontEnd/FrontIntegrador/src/components/contexts/BodyContext.jsx
+++ FrontEnd/FrontIntegrador/src/components/contexts/BodyContext.jsx
@@ -5,15 +5,26 @@ export const BodyContext = createContext()
 
 const BodyContextProvider = ({ children }) => {
   const [cars, setCars] = useState([])
+  const [localizaciones, setLocalizaciones] = useState([])
+
   useEffect(() => {
     axios.get("http://localhost:5000/cars")
       .then(res => setCars(res.data))
       .catch(err => console.log(err))
   }, [])
 
+  useEffect(() => {
+    axios.get("http://localhost:5000/localizaciones")
+      .then(res => setLocalizaciones(res.data))
+      .catch(err => console.log(err))
+  }, [])
+
+
   const data = {
     cars,
-    setCars
+    setCars,
+    localizaciones,
+    setLocalizaciones
   }
 
   return (
