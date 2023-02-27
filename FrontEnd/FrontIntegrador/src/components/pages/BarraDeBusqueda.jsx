0c16742444b9d25b1b12a2b3082145a6718e6819
Emilio Pino <pinoen@yahoo.com.ar>
2023-02-27 20:19:13 -0300

    se elimina state local de localizaciones y en su lugar se usa el array del body context

--- FrontEnd/FrontIntegrador/src/components/pages/BarraDeBusqueda.jsx
+++ FrontEnd/FrontIntegrador/src/components/pages/BarraDeBusqueda.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import { Typography, Box, useMediaQuery, MenuItem, FormControl, Select, OutlinedInput, Button } from '@mui/material';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
@@ -6,19 +6,21 @@ import colors from '../commons/colors';
 import DatePicker from "react-multi-date-picker"
 import { CustomInput } from '../commons/DatePickerCustomComponents';
 import Toolbar from 'react-multi-date-picker/plugins/toolbar';
+import { BodyContext } from '../contexts/BodyContext';
 
 
 
 const BarraDeBusqueda = () => {
     const isMobile = useMediaQuery('(max-width:600px)');
     const isTablet = useMediaQuery('(max-width:800px)');
-    const [listaLocalizaciones, setListaLocalizaciones] = useState([])
     const [localizacion, setLocalizacion] = useState({ provincia: "", pais: "" });
     const [dateRange, setDateRange] = useState([null, null]);
     const calendarRef = useRef();
 
+    const { localizaciones } = useContext(BodyContext)
+
     const handleChange = (event) => {
-        setLocalizacion(listaLocalizaciones[event.target.value]);
+        setLocalizacion(localizaciones[event.target.value]);
     }
 
     const handleSubmit = () => {
@@ -29,26 +31,20 @@ const BarraDeBusqueda = () => {
     const weekDays = ["S", "M", "T", "W", "T", "F", "S"];
     const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
 
-    useEffect(() => {
-        // Axios imaginario
-        setListaLocalizaciones([{ provincia: "San Carlos de Bariloche", pais: "Argentina" }, { provincia: "Buenos Aires", pais: "Argentina" }, { provincia: "Mendoza", pais: "Argentina" }, { provincia: "Cordoba", pais: "Argentina" }])
-    }, [])
-
-
     return (
         <Box display='flex' flexDirection={'column'} justifyContent='center' alignItems='center' width='100vw' marginTop={'60px'} backgroundColor={colors.c3} padding={"30px 0px"}>
-            <Typography variant='h1' fontSize={isMobile?'30px': '36px'} fontWeight="bold" color={colors.c4} textAlign="center" padding={'20px'} >Busca ofertas en Autos Economicos, Pick Ups y mucho más</Typography>
+            <Typography variant='h1' fontSize={isMobile ? '30px' : '36px'} fontWeight="bold" color={colors.c4} textAlign="center" padding={'20px'} >Busca ofertas en Autos Economicos, Pick Ups y mucho más</Typography>
 
             <FormControl sx={{ m: 1, width: isTablet ? '90%' : '70%', mt: 3, display: 'flex', justifyContent: 'center', alignItems: "center", flexDirection: isMobile ? 'column' : 'row', gap: '10px' }}>
                 <Select
                     displayEmpty
-                    sx={{ width: isMobile? '100%': '37.5%', height: '40px', backgroundColor: colors.background, borderRadius: '5px' }}
+                    sx={{ width: isMobile ? '100%' : '37.5%', height: '40px', backgroundColor: colors.background, borderRadius: '5px' }}
                     defaultValue=""
                     onChange={handleChange}
                     input={<OutlinedInput />}
                     renderValue={(selected) => {
 
-                        return <Box sx={{ display: 'flex', alignItems: 'center', color: selected === "" ? colors.fuenteBarraBusqueda: colors.c3, gap: '5px'}}>
+                        return <Box sx={{ display: 'flex', alignItems: 'center', color: selected === "" ? colors.fuenteBarraBusqueda : colors.c3, gap: '5px' }}>
                             <LocationOnIcon fontSize='medium' />
                             {selected === "" ? <Typography fontWeight={600} variant='subtitle1'>¿A dónde vamos?</Typography> :
                                 <Typography fontWeight={600} variant='subtitle1'>{localizacion.provincia}, {localizacion.pais}</Typography>}
@@ -56,7 +52,7 @@ const BarraDeBusqueda = () => {
                     }}
                     inputProps={{ 'aria-label': 'Without label' }}
                 >
-                    {listaLocalizaciones.map((locali, i) => (
+                    {localizaciones.map((locali, i) => (
                         <MenuItem value={i} key={i} >
                             <Box sx={{ display: 'flex', alignItems: 'center', borderBottom: `1.5px solid ${colors.principal}`, gap: '15px', padding: '5px', width: "100%" }}>
                                 <LocationOnOutlinedIcon fontSize='medium' />
@@ -70,8 +66,8 @@ const BarraDeBusqueda = () => {
                 </Select>
 
                 <DatePicker
-                    style={{width:'100%'}}
-                    containerStyle={{width: isMobile?'100%':'37.5%'}}
+                    style={{ width: '100%' }}
+                    containerStyle={{ width: isMobile ? '100%' : '37.5%' }}
                     ref={calendarRef}
                     value={dateRange}
                     onChange={setDateRange}
@@ -92,7 +88,7 @@ const BarraDeBusqueda = () => {
                     ]}
                 />
 
-                <Button sx={{ backgroundColor: colors.principal, color: colors.background, textTransform: 'none', width: isMobile? '100%': '25%', height: '40px', '&:hover': { backgroundColor: colors.principal } }}
+                <Button sx={{ backgroundColor: colors.principal, color: colors.background, textTransform: 'none', width: isMobile ? '100%' : '25%', height: '40px', '&:hover': { backgroundColor: colors.principal } }}
                     onClick={handleSubmit}>
                     Buscar
                 </Button>
