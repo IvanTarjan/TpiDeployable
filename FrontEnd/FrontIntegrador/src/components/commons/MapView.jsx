7f1b33583dbc744b69cb5529805ab7599bcd2bc4
Emilio Pino <pinoen@yahoo.com.ar>
2023-03-01 14:53:57 -0300

    se borra variable de position no necesaria

--- FrontEnd/FrontIntegrador/src/components/commons/MapView.jsx
+++ FrontEnd/FrontIntegrador/src/components/commons/MapView.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
-const position = [-32.8895, -68.8458]
-
 const MapView = ({ coordinates }) => {
   return (
     <MapContainer style={{ height: '500px', width: '95vw' }} center={coordinates} zoom={10} scrollWheelZoom={false} >
