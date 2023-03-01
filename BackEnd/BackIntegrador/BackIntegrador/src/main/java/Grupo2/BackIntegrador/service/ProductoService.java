7a1acf3bdd453220f4e8a32e5c2400b9fbf6795d
Pablo Velez <pablovelezlopera3@gmail.com>
2023-03-01 17:42:55 -0500

    Mejoras en productoService

--- BackEnd/BackIntegrador/BackIntegrador/src/main/java/Grupo2/BackIntegrador/service/ProductoService.java
+++ BackEnd/BackIntegrador/BackIntegrador/src/main/java/Grupo2/BackIntegrador/service/ProductoService.java
@@ -1,5 +1,6 @@
 package Grupo2.BackIntegrador.service;
 
+import Grupo2.BackIntegrador.Exception.ResourceNotFoundException;
 import Grupo2.BackIntegrador.model.Categoria;
 import Grupo2.BackIntegrador.model.Producto;
 import Grupo2.BackIntegrador.repository.ProductoRepository;
@@ -36,6 +37,20 @@ public class ProductoService {
     public Optional<Producto> buscarProductoXId(Long id){
         LOGGER.info("Se inició una operación de búsqueda del producto con id="+id);
         return productoRepository.findById(id);
+    }
+
+    public void eliminarProducto(Long id) throws ResourceNotFoundException {
+        Optional<Producto> productoaAEliminar=buscarProductoXId(id);
+        if (productoaAEliminar.isPresent()){
+            productoRepository.deleteById(id);
+            LOGGER.warn("Se realizo una operación de eliminación de un Producto con" +
+                    "id="+id);
+        }
+        else{
+            throw new ResourceNotFoundException("El producto a eliminar no existe" +
+                    " en la base de datos, se intentó encontrar sin éxito en id= "+id);
+        }
+
     }
 
 
