5ddfd8575d5ae3e40c0ccb6bbf7cc5f0d542482b
Emilio Pino <pinoen@yahoo.com.ar>
2023-03-01 18:36:18 -0300

    se agrega link para ver vehiculos segun categoria y se refactoriza componente

--- FrontEnd/FrontIntegrador/src/components/commons/CategoryCard.jsx
+++ FrontEnd/FrontIntegrador/src/components/commons/CategoryCard.jsx
@@ -5,14 +5,11 @@ import Typography from '@mui/material/Typography';
 import React from 'react'
 import colors from './colors';
 import styles from '../styles/Body.module.css'
+import { Link } from 'react-router-dom';
 
 const CategoryCard = ({ categoria, carImgs }) => {
 
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-    }
-
-    const randomImgIndex = getRandomInt(carImgs.length)
+    const randomImg = Math.floor(carImgs.length * Math.random())
 
     return (
         <Card sx={{ height: 380, width: 360 }}>
@@ -20,18 +17,17 @@ const CategoryCard = ({ categoria, carImgs }) => {
                 className={styles.categoryImages}
                 component="img"
                 sx={{ height: 280 }}
-                image={carImgs[randomImgIndex]}
+                image={carImgs[randomImg]}
                 alt={categoria.nombre}
             />
             <CardContent>
-                <Typography gutterBottom variant="h5" fontWeight={600} color={colors.c3}>
-                    {categoria.nombre}
+                <Typography gutterBottom variant="h5" fontWeight={600}>
+                    <Link to={`/category/${categoria.nombre}`} style={{ color: colors.c3 }} >{categoria.nombre}</Link>
                 </Typography>
                 <Typography variant="body2" fontWeight={600} color={colors.fuenteBarraBusqueda}>
                     {carImgs.length} vehiculos
                 </Typography>
             </CardContent>
-
         </Card>
     )
 }
