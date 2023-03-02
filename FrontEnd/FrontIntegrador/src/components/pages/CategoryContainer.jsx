53bd78a0478ccbb5a6b8fa0549ae062582768d7a
Emilio Pino <pinoen@yahoo.com.ar>
2023-03-02 16:30:16 -0300

    se refactoriza acorde a cambios en direccion de api para consumo de imagenes

--- FrontEnd/FrontIntegrador/src/components/pages/CategoryContainer.jsx
+++ FrontEnd/FrontIntegrador/src/components/pages/CategoryContainer.jsx
@@ -5,8 +5,8 @@ import colors from '../commons/colors'
 
 const CategoryContainer = ({ cars, categorias }) => {
 
-  const filterByCategoriaNombre = (nombre) => {
-    return cars.filter(car => car.category == nombre).map(carImg => carImg.image)
+  const filterByCategoryName = (nombre) => {
+    return cars.filter(car => car.category == nombre).map(carImg => carImg.image[0].original)
   };
 
   return (
@@ -14,7 +14,7 @@ const CategoryContainer = ({ cars, categorias }) => {
       <Typography variant='h3' display={"flex"} alignSelf={'flex-start'} fontSize="24px" fontWeight={600} color={colors.c3}>Buscar por tipo de vehiculo</Typography>
       <Box display={"flex"} flexDirection={{ xs: "column", sm: "row" }} gap={"15px"} justifyContent="center" alignItems={'center'} width='100%' flexWrap={"wrap"}>
         {categorias.map(categoria => (
-          <CategoryCard key={categoria.id} categoria={categoria} carImgs={filterByCategoriaNombre(categoria.nombre)} />
+          <CategoryCard key={categoria.id} categoria={categoria} carImgs={filterByCategoryName(categoria.nombre)} />
         ))}
       </Box>
     </Box>
