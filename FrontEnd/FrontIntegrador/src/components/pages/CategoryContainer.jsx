f4d8228d0dd39277e67d7433eac64ae306e5cf80
Emilio Pino <pinoen@yahoo.com.ar>
2023-03-03 15:38:05 -0300

    ligero cambio en gap entre tarjetas

--- FrontEnd/FrontIntegrador/src/components/pages/CategoryContainer.jsx
+++ FrontEnd/FrontIntegrador/src/components/pages/CategoryContainer.jsx
@@ -12,7 +12,7 @@ const CategoryContainer = ({ cars, categorias }) => {
   return (
     <Box display={"flex"} flexDirection="column" margin={"15px 0px"} justifyContent="center" alignItems="center">
       <Typography variant='h3' display={"flex"} alignSelf={'flex-start'} fontSize="24px" fontWeight={600} color={colors.c3}>Buscar por tipo de vehiculo</Typography>
-      <Box display={"flex"} flexDirection={{ xs: "column", sm: "row" }} gap={"15px"} justifyContent="center" alignItems={'center'} width='100%' flexWrap={"wrap"}>
+      <Box display={"flex"} flexDirection={{ xs: "column", sm: "row" }} gap={"5px"} justifyContent="center" alignItems={'center'} width='100%' flexWrap={"wrap"}>
         {categorias.map(categoria => (
           <CategoryCard key={categoria.id} categoria={categoria} carImgs={filterByCategoryName(categoria.nombre)} />
         ))}
