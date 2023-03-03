b08c67b40f4349efb2e5e686fb4580e8991c3e12
Pablo Velez <pablovelezlopera3@gmail.com>
2023-03-03 15:51:48 -0500

    Cambios de el ciudad repository

--- BackEnd/BackIntegrador/BackIntegrador/src/main/java/Grupo2/BackIntegrador/repository/CiudadRepository.java
+++ BackEnd/BackIntegrador/BackIntegrador/src/main/java/Grupo2/BackIntegrador/repository/CiudadRepository.java
@@ -3,6 +3,9 @@ package Grupo2.BackIntegrador.repository;
 import Grupo2.BackIntegrador.model.Ciudad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CiudadRepository extends JpaRepository<Ciudad, Long> {
 
+    Optional<Ciudad> findByNombre(String nombre);
 }
