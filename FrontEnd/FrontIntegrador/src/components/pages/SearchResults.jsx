2057a877fdddf03767cf533ddc67ea3bf4c9237d
Emilio Pino <pinoen@yahoo.com.ar>
2023-03-05 13:16:39 -0300

    se agrega funcionalidad para que filtre disponibilidad de vehiculos por ciudad

--- FrontEnd/FrontIntegrador/src/components/pages/SearchResults.jsx
+++ FrontEnd/FrontIntegrador/src/components/pages/SearchResults.jsx
@@ -8,14 +8,21 @@ import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { format } from 'date-fns'
 
 const SearchResults = () => {
-  const { cars } = useContext(BodyContext)
-  // const { city } = useParams()
+  const { cars, selectedCity } = useContext(BodyContext)
   const location = useLocation()
-
-  const [destination, setDestination] = useState(location.state.selectedCity)
   const [date, setDate] = useState(location.state.date)
 
-  // const neededCars = cars.filter(car => car.category == name)
+  const filterCarsByCity = (city) => {
+    const availableCities = []
+    for (let i = 0; i < cars.length; i++) {
+      for (let j = 0; j < cars[i].locations.length; j++) {
+        if (cars[i].locations[j] == city) {
+          availableCities.push(cars[i])
+        }
+      }
+    }
+    return availableCities
+  }
 
   const navigate = useNavigate()
   const handleClick = () => {
@@ -25,21 +32,21 @@ const SearchResults = () => {
     <>
       <div className={styles.headerCategory}>
         <div>
-          <h1 style={{ paddingTop: '0' }}>Vehiculos en {destination} entre los dias {format(date[0].startDate, 'dd/MM/yyyy')} y {format(date[0].endDate, 'dd/MM/yyyy')}</h1>
+          <h1 style={{ paddingTop: '0' }}>Vehiculos en {selectedCity} entre los dias {format(date[0].startDate, 'dd/MM/yyyy')} y {format(date[0].endDate, 'dd/MM/yyyy')}</h1>
         </div>
         <IconButton sx={{ width: 75 }} onClick={handleClick}>
           <ArrowBackIosNewIcon fontSize='large' color='action' />
         </IconButton>
       </div>
 
-      {/* <div className={styles.homeContainer}>
-          {neededCars.map(car => (
-            <VehicleCard
-              key={car.id}
-              car={car}
-            />
-          ))}
-        </div> */}
+      <div className={styles.homeContainer}>
+        {filterCarsByCity(selectedCity).map(car => (
+          <VehicleCard
+            key={car.id}
+            car={car}
+          />
+        ))}
+      </div>
     </>
   )
 }
