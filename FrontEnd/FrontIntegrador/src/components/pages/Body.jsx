1f9c7a466cced43176bf4bcab06436370f806acd
Emilio Pino <pinoen@yahoo.com.ar>
2023-03-06 18:32:09 -0300

    se prefiere barra de busqueda como componente

--- FrontEnd/FrontIntegrador/src/components/pages/Body.jsx
+++ FrontEnd/FrontIntegrador/src/components/pages/Body.jsx
@@ -4,14 +4,15 @@ import VehicleCard from '../commons/VehicleCard'
 import CategoryContainer from './CategoryContainer';
 import { Box } from '@mui/material';
 import { BodyContext } from '../contexts/BodyContext';
-import SearchingBar from './SearchingBar';
+import BarraDeBusqueda from '../pages/BarraDeBusqueda'
+
 
 const Body = () => {
   const { cars, categorias } = useContext(BodyContext)
 
   return (
     <Box display={"flex"} flexDirection="column" justifyContent={"center"} alignItems="center">
-      <SearchingBar />
+      <BarraDeBusqueda />
       <CategoryContainer cars={cars} categorias={categorias} />
       <div className={styles.homeContainer}>
         {cars.map(car => (
