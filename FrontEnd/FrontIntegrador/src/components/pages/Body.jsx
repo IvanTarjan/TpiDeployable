1b1595eba903da2381dc71b183f0ffd4025b98dd
Emilio Pino <pinoen@yahoo.com.ar>
2023-03-06 12:41:35 -0300

    uso de componente searching bar con libreria de calendario diferente

--- FrontEnd/FrontIntegrador/src/components/pages/Body.jsx
+++ FrontEnd/FrontIntegrador/src/components/pages/Body.jsx
@@ -1,17 +1,17 @@
 import React, { useContext } from 'react'
 import styles from '../styles/Body.module.css'
 import VehicleCard from '../commons/VehicleCard'
-import BarraDeBusqueda from "./BarraDeBusqueda";
 import CategoryContainer from './CategoryContainer';
 import { Box } from '@mui/material';
 import { BodyContext } from '../contexts/BodyContext';
+import SearchingBar from './SearchingBar';
 
 const Body = () => {
   const { cars, categorias } = useContext(BodyContext)
 
   return (
     <Box display={"flex"} flexDirection="column" justifyContent={"center"} alignItems="center">
-      <BarraDeBusqueda />
+      <SearchingBar />
       <CategoryContainer cars={cars} categorias={categorias} />
       <div className={styles.homeContainer}>
         {cars.map(car => (
