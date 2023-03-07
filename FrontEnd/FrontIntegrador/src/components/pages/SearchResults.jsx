63448ec7b567761079759a9d9827065a015d0c4b
Emilio Pino <pinoen@yahoo.com.ar>
2023-03-06 21:15:26 -0300

    se cambia titulo del encabezado

--- FrontEnd/FrontIntegrador/src/components/pages/SearchResults.jsx
+++ FrontEnd/FrontIntegrador/src/components/pages/SearchResults.jsx
@@ -32,25 +32,12 @@ const SearchResults = () => {
   }
   const availableCarsByCity = filterCarsByCity(selectedCity)
 
-  // const removeReservedCars = (avaCars, reservedCars, start, end) => {
-  //   for (let i = 0; i < avaCars.length; i++) {
-  //     for (let j = 0; j < reservedCars.length; j++) {
-  //       if (avaCars[i].id === reservedCars[j].car_id) {
-  //         if (start <= reservedCars[j].startDate && end >= reservedCars[j].endDate) {
-  //           avaCars.splice(avaCars.indexOf(avaCars[i]), 1)
-  //         }
-  //       }
-  //     }
-  //   }
-  //   return avaCars
-  // }
-  // const availableCarsByDate = removeReservedCars(availableCarsByCity, reservations, startDay, endDay)
-
   return (
     <>
       <div className={styles.headerCategory}>
         <div>
-          <h1 style={{ paddingTop: '0' }}>Vehiculos en {selectedCity} entre los dias {startDay} y {endDay}</h1>
+          <h1 style={{ paddingTop: '0' }}>Vehiculos disponibles en {selectedCity}</h1>
+          <h4 style={{ paddingLeft: '40px', color: 'white' }}>Haga click en boton VER MAS para consultar disponibilidad de fechas</h4>
         </div>
         <IconButton sx={{ width: 75 }} onClick={handleClick}>
           <ArrowBackIosNewIcon fontSize='large' color='action' />
