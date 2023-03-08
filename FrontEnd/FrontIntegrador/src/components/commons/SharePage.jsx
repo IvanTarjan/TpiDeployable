fbdcc039e08ef5a5e325b5921f18e2ce0060eb29
Emilio Pino <pinoen@yahoo.com.ar>
2023-03-08 20:36:39 -0300

    se crea componente para compartir pagina en redes sociales

---
+++ FrontEnd/FrontIntegrador/src/components/commons/SharePage.jsx
@@ -0,0 +1,53 @@
+import { IconButton, Modal } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react'
+import styles from '../styles/Body.module.css'
+import CloseIcon from '@mui/icons-material/Close';
+import { FacebookIcon, FacebookShareButton, LinkedinIcon, LinkedinShareButton, TwitterIcon, TwitterShareButton } from 'react-share';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 350,
+  height: 200,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+const SharePage = ({ open, handleClose, currentPageUrl }) => {
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box height={'600px'} sx={style}>
+        <div className={styles.closeBtnContainer}>
+          <IconButton sx={{ display: 'flex', justifyContent: 'flex-end' }} disableRipple={true} onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </div>
+        <div className={styles.shareContainer}>
+          <FacebookShareButton url={currentPageUrl}>
+            <FacebookIcon />
+          </FacebookShareButton>
+
+          <TwitterShareButton url={currentPageUrl}>
+            <TwitterIcon />
+          </TwitterShareButton>
+
+          <LinkedinShareButton url={currentPageUrl}>
+            <LinkedinIcon />
+          </LinkedinShareButton>
+        </div>
+      </Box>
+    </Modal>
+  )
+}
+
+export default SharePage
