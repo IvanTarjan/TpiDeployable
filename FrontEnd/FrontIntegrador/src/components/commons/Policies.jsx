e1d06405c4a48f5c80c540de5a2202c1eee3b323
Emilio Pino <pinoen@yahoo.com.ar>
2023-03-16 20:29:32 -0300

    se remueven componente ya no requeridos

--- FrontEnd/FrontIntegrador/src/components/commons/Policies.jsx
+++ FrontEnd/FrontIntegrador/src/components/commons/Policies.jsx
@@ -1,22 +1,17 @@
 import React from 'react'
-import SmokeFreeIcon from '@mui/icons-material/SmokeFree';
-import AddRoadIcon from '@mui/icons-material/AddRoad';
-import NoMealsIcon from '@mui/icons-material/NoMeals';
-import SosIcon from '@mui/icons-material/Sos';
-import RequestQuoteIcon from '@mui/icons-material/RequestQuote';
 import styles from '../styles/Body.module.css'
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 
-const Policies = ({selectedCar}) => {
+const Policies = ({ selectedCar }) => {
   return (
     <Box className={styles.iconsContainerDetail} >
 
-  {selectedCar.politica.map(item => (
-          <div key={item.id}>
-            <h4>{item.titulo}</h4>
-            <p>{item.descripcion}</p>
-          </div>
-        ))}
+      {selectedCar.politica.map(item => (
+        <div key={item.id}>
+          <h4>{item.titulo}</h4>
+          <p>{item.descripcion}</p>
+        </div>
+      ))}
     </Box>
   )
 }
