fd78c748c5ae6625861d2fe453ec660839c86723
Emilio Pino <pinoen@yahoo.com.ar>
2023-03-18 18:03:17 -0300

    ligero ajuste de dimensiones de CategoryCard

--- FrontEnd/FrontIntegrador/src/components/commons/CategoryCard.jsx
+++ FrontEnd/FrontIntegrador/src/components/commons/CategoryCard.jsx
@@ -12,7 +12,7 @@ const CategoryCard = ({ categoria, carImgs }) => {
     const randomImg = Math.floor(carImgs.length * Math.random())
 
     return (
-        <Card className={styles.categoryImagesContainer} sx={{ height: 380, width: 335 }}>
+        <Card className={styles.categoryImagesContainer} sx={{ height: 320, width: 350 }}>
             <CardMedia
                 component="img"
                 sx={{ height: "220px", width: '300px' }}
