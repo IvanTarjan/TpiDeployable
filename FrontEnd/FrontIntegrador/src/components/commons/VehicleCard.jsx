e9d3c9f4759be08da19ff488839630c5836d4d48
Emilio Pino <pinoen@yahoo.com.ar>
2023-03-18 18:03:48 -0300

    ligero ajuste de dimensiones de VehicleCard

--- FrontEnd/FrontIntegrador/src/components/commons/VehicleCard.jsx
+++ FrontEnd/FrontIntegrador/src/components/commons/VehicleCard.jsx
@@ -26,7 +26,7 @@ export default function VehicleCard({ car }) {
     }}>
       <Box className={styles.imageContainer}>
         <CardMedia className={styles.carImages}
-          sx={{ height: 280, width: 375 }}
+          sx={{ height: 220, width: 390 }}
           image={car.imagen[0].url_img}
           title={car.titulo}
         />
