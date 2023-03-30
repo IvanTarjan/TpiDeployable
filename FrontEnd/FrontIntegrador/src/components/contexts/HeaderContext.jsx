07fd7185b37c590a2be564442a131c44a8a592bd
Emilio Pino <pinoen@yahoo.com.ar>
2023-03-30 15:49:45 -0300

    se elimina estado para users del context

--- FrontEnd/FrontIntegrador/src/components/contexts/HeaderContext.jsx
+++ FrontEnd/FrontIntegrador/src/components/contexts/HeaderContext.jsx
@@ -1,26 +1,17 @@
 import axios from 'axios';
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext, useState } from 'react'
 
 export const HeaderContext = createContext()
 
 const HeaderContextProvider = ({ children }) => {
 
   const [headerType, setHeaderType] = useState('initial');
-  const [users, setUsers] = useState([])
   const [isLog, setIsLog] = useState(false)
   const [currentUser, setCurrentUser] = useState(undefined)
-
-  useEffect(() => {
-    axios.get("http://ec2-3-138-67-153.us-east-2.compute.amazonaws.com:8080/usuario")
-      .then(res => setUsers(res.data))
-      .catch(err => console.log(err))
-  }, [])
 
   const data = {
     headerType,
     setHeaderType,
-    users,
-    setUsers,
     isLog,
     setIsLog,
     currentUser,
