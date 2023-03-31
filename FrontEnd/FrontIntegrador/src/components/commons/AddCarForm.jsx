f47c6c608fe0478201bfab63a54ce6a31878b3b4
Emilio Pino <pinoen@yahoo.com.ar>
2023-03-31 12:42:47 -0300

    se completa onSubmit function

--- FrontEnd/FrontIntegrador/src/components/commons/AddCarForm.jsx
+++ FrontEnd/FrontIntegrador/src/components/commons/AddCarForm.jsx
@@ -1,23 +1,199 @@
-import React, { useContext } from 'react'
-import { Button, FormHelperText, Grid, IconButton, MenuItem, Select, TextField, useMediaQuery } from '@mui/material'
-import AddBoxIcon from '@mui/icons-material/AddBox';
+import React, { useContext, useState } from 'react'
+import { Button, FormHelperText, Grid, MenuItem, Select, TextField, useMediaQuery } from '@mui/material'
 import styles from '../styles/Body.module.css'
 import SendIcon from '@mui/icons-material/Send';
 import { BodyContext } from '../contexts/BodyContext';
-import AttributesSelector from './AttributesSelector';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+import { HeaderContext } from '../contexts/HeaderContext';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import Chip from '@mui/material/Chip';
 
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const attributes = [
+  'Nafta',
+  'Diesel',
+  'Diesel premium',
+  'Caja Manual',
+  'Caja Automatica',
+  'Frenos a disco',
+  'Frenos con ABS',
+  'Aire Acondicionado',
+  '2 Airbags',
+];
+
+function getStyles(attribute, attributeName, theme) {
+  return {
+    fontWeight:
+      attributeName.indexOf(attribute) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
 const AddCarForm = () => {
 
   const { categorias, localizaciones } = useContext(BodyContext)
+  const { currentUser } = useContext(HeaderContext)
+  // console.log(currentUser)
+  const theme = useTheme();
+  const [attributeName, setAttributeName] = useState([]);
 
-  let carCategory = document.querySelector('#categoria')
-  let carCity = document.querySelector('#ciudad')
+  const handleAttributesChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setAttributeName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+  const { handleSubmit, handleChange, values, errors } = useFormik({
+    initialValues: {
+      name: '',
+      category: '',
+      price: 0,
+      city: '',
+      description: '',
+      house: '',
+      safety: '',
+      cancel: '',
+      mainPhoto: '',
+      sidePhoto: '',
+      rearPhoto: '',
+      trunkPhoto: '',
+      insidePhoto: '',
+      allAttributes: attributeName
+    },
+    validateOnChange: true,
+    validationSchema: Yup.object({
+      name: Yup.string().required('Campo obligatorio'),
+      category: Yup.string().required('Campo obligatorio'),
+      price: Yup.number().required('Campo obligatorio').positive('El precio debe ser un numero positivo'),
+      city: Yup.string().required('Campo obligatorio'),
+      description: Yup.string().required('Campo obligatorio'),
+      house: Yup.string().required('Campo obligatorio'),
+      safety: Yup.string().required('Campo obligatorio'),
+      cancel: Yup.string().required('Campo obligatorio'),
+      mainPhoto: Yup.string().url().required('Debe cargar foto principal'),
+      sidePhoto: Yup.string().url().required('Debe cargar foto lateral'),
+    }),
+    onSubmit: (data) => axios.post('http://ec2-3-138-67-153.us-east-2.compute.amazonaws.com:8080/api/producto', {
+      titulo: data.name,
+      descripcion: data.description,
+      precio: data.price,
+      imagen:
+        [{
+          titulo: 'Main',
+          url_img: data.mainPhoto
+        },
+        {
+          titulo: 'Atras',
+          url_img: data.rearPhoto
+        },
+        {
+          titulo: 'Baul',
+          url_img: data.trunkPhoto
+        },
+        {
+          titulo: 'Interior',
+          url_img: data.insidePhoto
+        },
+        {
+          titulo: 'Side',
+          url_img: data.sidePhoto
+        }],
+      politica: [
+        {
+          titulo: 'Devolucion',
+          descripcion: data.house
+        },
+        {
+          titulo: 'Entrega',
+          descripcion: data.safety
+        },
+        {
+          titulo: 'Uso',
+          descripcion: data.cancel
+        }
+      ],
+      categoria: [
+        {
+          titulo: data.category,
+          descripcion: data.description,
+          url_img: ''
+        }
+      ],
+      ubicacion: [
+        {
+          nombre: data.city,
+          pais: "Argentina"
+        }
+      ],
+      caracteristica: [
+        {
+          titulo: "2 Airbags",
+          icono: "https://bucket-grupo2-img.s3.us-east-2.amazonaws.com/Caracteristica-iconos/airbag-svgrepo-com.svg"
+        },
+        {
+          titulo: data.allAttributes.includes("Nafta") ? "Nafta" : "Diesel premium",
+          icono: "https://bucket-grupo2-img.s3.us-east-2.amazonaws.com/Caracteristica-iconos/fuel-svgrepo-com.svg"
+        },
+        {
+          titulo: "Aire Acondicionado",
+          icono: "https://bucket-grupo2-img.s3.us-east-2.amazonaws.com/Caracteristica-iconos/air-conditioning-svgrepo-com.svg"
+        },
+        {
+          titulo: data.allAttributes.includes("Caja Manual") ? "Caja Manual" : "Caja Automatica",
+          icono: "https://bucket-grupo2-img.s3.us-east-2.amazonaws.com/Caracteristica-iconos/gearshift-svgrepo-com.svg"
+        },
+        {
+          titulo: data.allAttributes.includes("Frenos con ABS") ? "Frenos con ABS" : "Frenos a disco",
+          icono: "https://bucket-grupo2-img.s3.us-east-2.amazonaws.com/Caracteristica-iconos/breaks-car-svgrepo-com.svg"
+        }
+      ]
+    },
+      {
+        headers: {
+          'Authorization': `${currentUser.tokenType} ${currentUser.accessToken}`
+        }
+      }
+    ).then(res => {
+      Swal.fire({
+        title: 'Registro exitoso!',
+        text: 'El vehiculo creado ha sido cargado al home',
+        icon: 'success',
+        confirmButtonColor: '#1DBEB4',
+        timer: 3000
+      })
+      nagivate('/')
+    }).catch(err => {
+      console.log(err)
+      Swal.fire({
+        title: 'Error!',
+        text: 'Lamentablemente no ha crear el vehiculo. Por favor intente más tarde',
+        icon: 'error',
+        confirmButtonColor: '#1DBEB4',
+      })
+    })
+  })
 
   const isMobile = useMediaQuery('(max-width:640px)');
 
-  const handleAddIcon = () => {
-    console.log('Agregando icono')
-  }
   return (
     <div className={styles.addCar}>
       <h1>Crear vehiculo</h1>
@@ -26,7 +202,7 @@ const AddCarForm = () => {
         backgroundColor: 'white',
         boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
         borderRadius: '8px',
-        padding: '10px 10px',
+        padding: '10px 20px',
         width: '100%',
         height: 'auto'
       }} container spacing={2}>
@@ -34,6 +210,11 @@ const AddCarForm = () => {
           <TextField
             label='Nombre del vehiculo'
             type={'text'}
+            name='name'
+            value={values.name}
+            onChange={handleChange}
+            error={errors.name ? true : false}
+            helperText={errors.name}
             fullWidth
           />
         </Grid>
@@ -41,7 +222,11 @@ const AddCarForm = () => {
           <Select
             defaultValue={''}
             fullWidth
-            id='categoria'
+            name='category'
+            value={values.category}
+            onChange={handleChange}
+            error={errors.category ? true : false}
+            helperText={errors.category}
           >
             <MenuItem value="">
               <em style={{ color: '#383B58', fontWeight: '700' }}>Categoria</em>
@@ -56,15 +241,25 @@ const AddCarForm = () => {
         </Grid>
         <Grid item xs={isMobile ? 12 : 6}>
           <TextField
-            label='Direccion'
-            type={'text'}
+            label='Precio'
+            value={values.price}
+            placeholder='$'
+            name='price'
+            onChange={handleChange}
+            error={errors.price ? true : false}
+            helperText={errors.price}
+            type={'number'}
             fullWidth />
         </Grid>
         <Grid item xs={isMobile ? 12 : 6}>
           <Select
             defaultValue={''}
             fullWidth
-            id='ciudad'
+            name='city'
+            value={values.city}
+            onChange={handleChange}
+            error={errors.city ? true : false}
+            helperText={errors.city}
           >
             <MenuItem value="">
               <em style={{ color: '#383B58', fontWeight: '700' }}>Ciudad</em>
@@ -79,6 +274,11 @@ const AddCarForm = () => {
         <Grid item xs={isMobile ? 12 : 12}>
           <TextField
             label="Descripcion"
+            name='description'
+            value={values.description}
+            onChange={handleChange}
+            error={errors.description ? true : false}
+            helperText={errors.description}
             placeholder='Escriba aqui'
             multiline
             type={'text'}
@@ -93,13 +293,44 @@ const AddCarForm = () => {
         backgroundColor: '#f9f9f9',
         boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
         borderRadius: '8px',
-        padding: '10px 10px',
+        padding: '10px 20px',
         width: '100%',
         height: 'auto'
       }}
         container spacing={2} >
         <Grid item xs={isMobile ? 12 : 12}>
-          <AttributesSelector />
+
+          <InputLabel id="demo-multiple-chip-label">Atributos</InputLabel>
+          <Select
+            fullWidth
+            labelId="demo-multiple-chip-label"
+            id="demo-multiple-chip"
+            multiple
+            name='allAttributes'
+            value={attributeName}
+            onChange={handleAttributesChange}
+            input={<OutlinedInput id="select-multiple-chip" label="Atributos" />}
+            renderValue={(selected) => (
+              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                {selected.map((value) => (
+                  <Chip key={value} label={value} />
+                ))}
+              </Box>
+            )}
+            MenuProps={MenuProps}
+          >
+            {attributes.map((attribute) => (
+              <MenuItem
+                key={attribute}
+                value={attribute}
+                style={getStyles(attribute, attributeName, theme)}
+              >
+                {attribute}
+              </MenuItem>
+            ))}
+          </Select>
+
+
         </Grid>
       </Grid >
 
@@ -108,7 +339,7 @@ const AddCarForm = () => {
         backgroundColor: '#fff',
         boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
         borderRadius: '8px',
-        padding: '10px 10px',
+        padding: '10px 20px',
         width: '100%',
         height: 'auto'
       }}
@@ -116,6 +347,11 @@ const AddCarForm = () => {
         <Grid item xs={isMobile ? 12 : 4}>
           <TextField
             label='Normas de la casa'
+            name='house'
+            value={values.house}
+            onChange={handleChange}
+            error={errors.house ? true : false}
+            helperText={errors.house}
             multiline
             minRows={8}
             placeholder='Escriba aqui'
@@ -126,6 +362,11 @@ const AddCarForm = () => {
         <Grid item xs={isMobile ? 12 : 4}>
           <TextField
             label='Salud y Seguridad'
+            name='safety'
+            value={values.safety}
+            onChange={handleChange}
+            error={errors.safety ? true : false}
+            helperText={errors.safety}
             multiline
             minRows={8}
             placeholder='Escriba aqui'
@@ -136,6 +377,11 @@ const AddCarForm = () => {
         <Grid item xs={isMobile ? 12 : 4}>
           <TextField
             label='Politica de cancelacion'
+            name='cancel'
+            value={values.cancel}
+            onChange={handleChange}
+            error={errors.cancel ? true : false}
+            helperText={errors.cancel}
             multiline
             minRows={8}
             placeholder='Escriba aqui'
@@ -148,28 +394,80 @@ const AddCarForm = () => {
           backgroundColor: '#f9f9f9',
           boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
           borderRadius: '8px',
-          padding: '10px 10px',
+          padding: '10px 20px',
           width: '100vw',
           height: 'auto'
         }}
           container spacing={2} >
 
-          <Grid item xs={isMobile ? 12 : 12}>
+          <Grid item xs={isMobile ? 12 : 8}>
             <TextField
-              InputProps={{
-                endAdornment: (
-                  <IconButton color='primary' disableRipple={true} onClick={handleAddIcon}>
-                    <AddBoxIcon style={{ fontSize: '40px' }} />
-                  </IconButton>
-                ),
-              }}
-              label='Insertar https://'
+              label='Foto principal'
+              name='mainPhoto'
+              value={values.mainPhoto}
+              onChange={handleChange}
+              error={errors.mainPhoto ? true : false}
+              helperText={errors.mainPhoto}
+              placeholder='Insertar https://'
               type={'text'}
               fullWidth />
           </Grid>
+
+          <Grid item xs={isMobile ? 12 : 4}>
+            <TextField
+              label='Lateral'
+              name='sidePhoto'
+              value={values.sidePhoto}
+              onChange={handleChange}
+              error={errors.sidePhoto ? true : false}
+              helperText={errors.sidePhoto}
+              placeholder='Insertar https://'
+              type={'text'}
+              fullWidth />
+          </Grid>
+
+          <Grid item xs={isMobile ? 12 : 4}>
+            <TextField
+              label='Vista posterior'
+              name='rearPhoto'
+              value={values.rearPhoto}
+              onChange={handleChange}
+              error={errors.rearPhoto ? true : false}
+              helperText={errors.rearPhoto}
+              placeholder='Insertar https://'
+              type={'text'}
+              fullWidth />
+          </Grid>
+
+          <Grid item xs={isMobile ? 12 : 4}>
+            <TextField
+              label='Baulera'
+              name='trunkPhoto'
+              value={values.trunkPhoto}
+              onChange={handleChange}
+              error={errors.trunkPhoto ? true : false}
+              helperText={errors.trunkPhoto}
+              placeholder='Insertar https://'
+              type={'text'}
+              fullWidth />
+          </Grid>
+
+          <Grid item xs={isMobile ? 12 : 4}>
+            <TextField
+              label='Interior'
+              name='insidePhoto'
+              value={values.insidePhoto}
+              onChange={handleChange}
+              error={errors.insidePhoto ? true : false}
+              helperText={errors.insidePhoto}
+              placeholder='Insertar https://'
+              type={'text'}
+              fullWidth />
+          </Grid>
+
         </Grid >
         <Grid item xs={isMobile ? 12 : 12} sx={{ display: 'flex', justifyContent: 'center' }} >
-          <Button sx={{ marginBottom: '130px', width: '450px', marginTop: '30px' }} size='large' color="primary" type='submit' variant='contained' endIcon={<SendIcon />}>Crear</Button>
+          <Button onSubmit={handleSubmit} sx={{ marginBottom: '130px', width: '450px', marginTop: '30px' }} size='large' color="primary" type='submit' variant='contained' endIcon={<SendIcon />}>Crear</Button>
         </Grid>
       </Grid >
     </div >
